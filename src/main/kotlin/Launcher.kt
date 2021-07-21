54b3a63a420fc98f186c8d2ffbd3d1a2c4cab106
AHpx <AHpx@yandex.com>
2021-07-21 12:12:40 +0800

    Listen all message type

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -7,7 +7,10 @@ import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.events.MessageEvent
 import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.message.data.SimpleServiceMessage
+import net.mamoe.mirai.utils.MiraiExperimentalApi
 
+@MiraiExperimentalApi
 suspend fun main(args : Array<String>){
     val bot = BotFactory.newBot(args[0].toLong(), args[1])
     bot.login()
@@ -15,17 +18,15 @@ suspend fun main(args : Array<String>){
     val modules = listOf<CommandBase>(BasicCommandModule(), GithubThumbnailModule())
 
     GlobalEventChannel.subscribeAlways<MessageEvent> {
-        if (it.message.any{ x -> x is PlainText }){
-            for (text in it.message.filterIsInstance<PlainText>()) {
-                for (module in modules) {
-                    val predicate = when (module.executorType) {
-                        CommandExecuteType.Contains -> {s:String -> text.content.lowercase().contains(s)}
-                        CommandExecuteType.Equals -> {s: String -> text.content == s}
-                    }
+        for (text in it.message) {
+            for (module in modules) {
+                val predicate = when (module.executorType) {
+                    CommandExecuteType.Contains -> {s:String -> text.contentToString().lowercase().contains(s)}
+                    CommandExecuteType.Equals -> {s: String -> text.contentToString() == s}
+                }
 
-                    if (module.executors.any(predicate)){
-                        module.execute(text.content, it.subject)
-                    }
+                if (module.executors.any(predicate)){
+                    module.execute(text.contentToString(), it.subject)
                 }
             }
         }
