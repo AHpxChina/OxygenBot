46b20352b0cc176a28606e7db3e878516a152284
AHpx <AHpx@yandex.com>
2021-07-24 00:31:06 +0800

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.21"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "cn.ahpxchina.oxygen.bot"
@@ -13,10 +14,16 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     val miraiVersion = "2.6.7"
-    api("net.mamoe", "mirai-core-api", miraiVersion)     // 编译代码使用
-    runtimeOnly("net.mamoe", "mirai-core", miraiVersion) // 运行时使用
+    api("net.mamoe", "mirai-core-api", miraiVersion)
+    runtimeOnly("net.mamoe", "mirai-core", miraiVersion)
 
     implementation ("org.jsoup:jsoup:1.14.1")
     implementation ("com.google.code.gson:gson:2.8.7")
     implementation ("org.reflections:reflections:0.9.12")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>() {
+    manifest {
+        attributes["Main-Class"] = "LauncherKt"
+    }
 }
