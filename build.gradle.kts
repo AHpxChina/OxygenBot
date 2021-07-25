f422488b1c18e82a9903f35518cffb840436ef35
AHpx <AHpx@yandex.com>
2021-07-25 20:43:33 +0800

    Add azure tts sdk...but useless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.gradle.kotlin.dsl.accessors.runtime.extensionOf
+
 plugins {
     kotlin("jvm") version "1.5.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    java
 }
 
 group = "cn.ahpxchina.oxygen.bot"
@@ -8,6 +11,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://csspeechstorage.blob.core.windows.net/maven/")
 }
 
 dependencies {
@@ -20,6 +24,8 @@ dependencies {
     implementation ("org.jsoup:jsoup:1.14.1")
     implementation ("com.google.code.gson:gson:2.8.7")
     implementation ("org.reflections:reflections:0.9.12")
+
+    implementation ("com.microsoft.cognitiveservices.speech", "client-sdk", "1.17.0", ext = "jar")
 }
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>() {
